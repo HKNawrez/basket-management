0a43d2dc92ee013bab16864ac278c00ea9c143be
nawrez.elhadj-khelil <nawrez.elhadj-khelil@talan.com>
2025-03-05 15:05:43 +0100

    [kata] remove empty list component from route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,11 @@
 import { Routes } from '@angular/router';
 import { BasketComponent } from './components/basket/basket.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
-import { EmptyBasketComponent } from './components/empty-basket/empty-basket.component';
 
 export const routes: Routes = [
   {
     path: 'product-list',
     loadComponent: () => import('./components/product-list/product-list.component').then(m => m.ProductListComponent)
-  },
-  {
-    path: 'empty-basket',
-    loadComponent: () => import('./components/empty-basket/empty-basket.component').then(m => m.EmptyBasketComponent)
   },
   {
     path: 'basket',
